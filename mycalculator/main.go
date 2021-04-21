a0e12c9098bb07548e7cb5c4cc38c809a2d497c1
kaustubh7 <kaustubh7@gmail.com>
2021-04-21 15:17:26 -0400

    Adding UI to calculator

--- mycalculator/main.go
+++ mycalculator/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
+	"regexp"
 	"strconv"
+	"text/template"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -33,18 +35,19 @@ type PrevResult struct {
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	if len(request.Body) > 0 && request.Body != "clear" {
-		return writeToResponse(cleanUpAndCreateResponseJson(string(request.Body), eval(string(request.Body))))
-	} else if request.Body == "clear" {
-		return writeToResponse(cleanUpAndCreateResponseJson(string(request.Body), 0))
+	var expression = request.QueryStringParameters["expression"]
+	if expression == "clear" {
+		return writeToResponse(clearResults())
 	} else {
-		return writeToResponse(Result{})
+		return writeToResponse(cleanUpAndCreateResponseJson(expression, eval(expression)))
 	}
 }
 
 func eval(inputExpression string) float64 {
 	var operator = "+"
+	if len(inputExpression) == 0 {
+		return 0
+	}
 	for i := 0; i < len(inputExpression); i++ {
 		var char string = string(inputExpression[i])
 
@@ -92,29 +95,38 @@ func eval(inputExpression string) float64 {
 	return result
 }
 
+func BuildPage(htmlTemplate string, result Result) *bytes.Buffer {
+	var bodyBuffer bytes.Buffer
+	t := template.New("template")
+	var templates = template.Must(t.Parse(htmlTemplate))
+	templates.Execute(&bodyBuffer, result)
+	return &bodyBuffer
+}
+
 func cleanUpAndCreateResponseJson(body string, total float64) Result {
 	numStack = numStack[:0]
-	if len(body) > 0 && body != "clear" {
+	isValidExpr := regexp.MustCompile(`[0-9\\+\\-\\*\\/\\(\\)]+$`).MatchString
+	if isValidExpr(body) {
 		prevResult := PrevResult{string(body), total}
 		prevResults = append(prevResults, prevResult)
 		return Result{prevResults, globalResult, "Please enter next expression to calculate"}
 	} else {
-		prevResults = prevResults[:0]
-		globalResult = 0
-		return Result{prevResults, globalResult, "history is cleared!"}
+		return Result{prevResults, globalResult, "Enter a valid string to continue"}
 	}
 }
 
+func clearResults() Result {
+	prevResults = prevResults[:0]
+	globalResult = 0
+	return Result{prevResults, globalResult, "history is cleared!"}
+
+}
+
 func writeToResponse(result Result) (events.APIGatewayProxyResponse, error) {
-	if result.Message != "" {
-		jsonRes, err := json.Marshal(result)
-		if err != nil {
-			return events.APIGatewayProxyResponse{Body: "Server error occured", StatusCode: 500}, nil
-		}
-		return events.APIGatewayProxyResponse{Body: string(jsonRes), StatusCode: 200}, nil
-	} else {
-		return events.APIGatewayProxyResponse{Body: "Please check input!", StatusCode: 200}, nil
-	}
+	return events.APIGatewayProxyResponse{
+		Headers:    map[string]string{"content-type": "text/html"},
+		Body:       BuildPage(HtmlTemplate, result).String(),
+		StatusCode: 200}, nil
 }
 
 func writeToNumStack(operand string, operator string) int {
@@ -154,3 +166,217 @@ func isNumber(s string) bool {
 func main() {
 	lambda.Start(Handler)
 }
+
+var HtmlTemplate string = `<html>
+<head>
+	<style type='text/css'>
+	@import url('https://fonts.googleapis.com/css?family=Poppins');
+
+	/* BASIC */
+	
+	html {
+	  background-color: #56baed;
+	}
+	
+	body {
+	  font-family: 'Poppins', sans-serif;
+	  height: 100vh;
+	}
+	
+	a {
+	  color: #92badd;
+	  display:inline-block;
+	  text-decoration: none;
+	  font-weight: 400;
+	}
+	
+	h2 {
+	  text-align: center;
+	  font-size: 16px;
+	  font-weight: 600;
+	  text-transform: uppercase;
+	  display:inline-block;
+	  margin: 40px 8px 10px 8px; 
+	  color: #cccccc;
+	}
+	table {
+	  text-align: center;
+	  font-size: 14px;
+	  font-weight: 200;
+	  text-transform: uppercase;
+	  display:inline-block;
+	  margin: 10px 8px 10px 8px; 
+	  color: #0d0d0d;
+	}
+	p {
+	  text-align: center;
+	  font-size: 14px;
+	  font-weight: 200;
+	  margin: 40px 8px 10px 8px; 
+	  color: #f30b0b;
+	}
+	
+	
+	
+	/* STRUCTURE */
+	
+	.wrapper {
+	  display: flex;
+	  align-items: center;
+	  flex-direction: column; 
+	  justify-content: center;
+	  width: 100%;
+	  min-height: 100%;
+	  padding: 20px;
+	}
+	
+	#formContent {
+	  -webkit-border-radius: 10px 10px 10px 10px;
+	  border-radius: 10px 10px 10px 10px;
+	  background: #fff;
+	  padding: 30px;
+	  width: 90%;
+	  max-width: 450px;
+	  position: relative;
+	  padding: 0px;
+	  -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+	  box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+	  text-align: center;
+	}
+	
+	#formFooter {
+	  background-color: #f6f6f6;
+	  border-top: 1px solid #dce8f1;
+	  padding: 25px;
+	  text-align: center;
+	  -webkit-border-radius: 0 0 10px 10px;
+	  border-radius: 0 0 10px 10px;
+	}
+	
+	
+	
+	/* TABS */
+	
+	h2.inactive {
+	  color: #cccccc;
+	}
+	
+	h2.active {
+	  color: #0d0d0d;
+	  border-bottom: 2px solid #5fbae9;
+	}
+	
+	
+	
+	/* FORM TYPOGRAPHY*/
+	
+	input[type=button], input[type=submit], input[type=reset]  {
+	  background-color: #56baed;
+	  border: none;
+	  color: white;
+	  padding: 15px 80px;
+	  text-align: center;
+	  text-decoration: none;
+	  display: inline-block;
+	  text-transform: uppercase;
+	  font-size: 13px;
+	  -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
+	  box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
+	  -webkit-border-radius: 5px 5px 5px 5px;
+	  border-radius: 5px 5px 5px 5px;
+	  margin: 5px 20px 40px 20px;
+	  -webkit-transition: all 0.3s ease-in-out;
+	  -moz-transition: all 0.3s ease-in-out;
+	  -ms-transition: all 0.3s ease-in-out;
+	  -o-transition: all 0.3s ease-in-out;
+	  transition: all 0.3s ease-in-out;
+	}
+	
+	input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover  {
+	  background-color: #39ace7;
+	}
+	
+	input[type=button]:active, input[type=submit]:active, input[type=reset]:active  {
+	  -moz-transform: scale(0.95);
+	  -webkit-transform: scale(0.95);
+	  -o-transform: scale(0.95);
+	  -ms-transform: scale(0.95);
+	  transform: scale(0.95);
+	}
+	
+	input[type=text] {
+	  background-color: #f6f6f6;
+	  border: none;
+	  color: #0d0d0d;
+	  padding: 15px 32px;
+	  text-align: center;
+	  text-decoration: none;
+	  display: inline-block;
+	  font-size: 16px;
+	  margin: 5px;
+	  width: 85%;
+	  border: 2px solid #f6f6f6;
+	  -webkit-transition: all 0.5s ease-in-out;
+	  -moz-transition: all 0.5s ease-in-out;
+	  -ms-transition: all 0.5s ease-in-out;
+	  -o-transition: all 0.5s ease-in-out;
+	  transition: all 0.5s ease-in-out;
+	  -webkit-border-radius: 5px 5px 5px 5px;
+	  border-radius: 5px 5px 5px 5px;
+	}
+	
+	input[type=text]:focus {
+	  background-color: #fff;
+	  border-bottom: 2px solid #5fbae9;
+	}
+	
+	input[type=text]:placeholder {
+	  color: #cccccc;
+	  font-size: 12px;
+	}            
+	
+	/* OTHERS */
+	
+	*:focus {
+		outline: none;
+	}
+	
+	* {
+	  box-sizing: border-box;
+	}
+</style>
+</head>
+<body>
+   
+
+	<div class='wrapper fadeIn'>
+	<div id='formContent'>
+		<!-- Tabs Titles -->
+		<h2 class='active'> Calculator </h2>
+
+		<!-- Login Form -->
+		<form action='https://a3vjqgmuxh.execute-api.us-east-1.amazonaws.com/mycalculator' method='get'>
+		<input type='text' id='login' class='fadeIn second' name='expression' placeholder='enter expression here and hit enter'>
+		<h4>Previous results</h4>
+		{{if .PreviousResult}}
+		<table type='text' class='fadeIn second'>{{range $y, $x := .PreviousResult }}
+			<tr>
+			<td>{{ $x.Input }}</td>
+			<td> = </td>
+			<td>{{ $x.Total }}</td>
+			</tr>{{end}}
+		</table>
+		<h4>Total</h4>
+		<h3 type='text' class='fadeIn second'>{{.FinalResult}}</h3>
+		{{end}}
+		<p type='text' class='fadeIn second'><b>{{.Message}}</b></p>
+		</form>
+		<form action='https://a3vjqgmuxh.execute-api.us-east-1.amazonaws.com/mycalculator' method='get'>
+			<input type='hidden' id='login' class='fadeIn second' name='expression' value='clear'>
+			<input type='submit' value='Clear Results'>
+		</form>
+
+	</div>
+	</div>
+</body>
+</html>`
